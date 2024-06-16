d43990e176a38a7ab39ca1cc0be0c46b6cba8c49
Rolando HR <rolando167@hotmail.com>
2024-06-16 14:21:33 +0200

    RH 16 - ca

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -3,11 +3,17 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { DatabaseModule } from 'src/core/config/databases/database.module';
 import { PostgresUserRepository } from './repository/postgres.user.repository';
+import { UserRepository } from './repository/dependency.repository';
+import { MongoUserRepository } from './repository/mongo.user.repository';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [UserController],
-    providers: [UserService, PostgresUserRepository],
+    providers: [UserService
+        , PostgresUserRepository
+        , UserRepository
+        , MongoUserRepository
+    ],
 })
 
 export class UserModule { }
